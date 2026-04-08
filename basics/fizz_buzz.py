38822bed00956ccaa9beaa996f597f84e00337d2
Chris Jones <chrisj96@proton.me>
2026-04-08 16:51:47 +0100

    Add code for fizzbuzz

--- basics/fizz_buzz.py
+++ basics/fizz_buzz.py
@@ -1,17 +1,12 @@
-package basics;
+def fizz_buzz(n):
+    if n % 15 == 0:
+        return "FizzBuzz"
+    if n % 3 == 0:
+        return "Fizz"
+    if n % 5 == 0:
+        return "Buzz"
+    return str(n)
 
-public class FizzBuzz {
-    
-    public static String fizzBuzz(int n) {
-        if (n % 15 == 0) return "FizzBuzz";
-        if (n % 3 == 0) return "Fizz";
-        if (n % 5 == 0) return "Buzz";
-        return String.valueOf(n);
-    }
-
-    public static void main(String[] args) {
-        for (int i = 1; i <= 20; i++) {
-            System.out.println(fizzBuzz(i));
-        }
-    }
-}
+if __name__ == "__main__":
+    for i in range(1, 21):
+        print(fizz_buzz(i))
